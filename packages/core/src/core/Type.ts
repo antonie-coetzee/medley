a6d39d09b10fcaa35de077da692b518c3520cfdc
antonie-coetzee <mail@acoetzee.com>
2021-12-27 07:35:20 +0200

    wip

--- packages/core/src/core/Type.ts
+++ packages/core/src/core/Type.ts
@@ -4,15 +4,10 @@ import { Scoped } from "./Scoped";
 export interface TypeVersion<MModule extends Module> {
   version: string;
   module: MModule;
-  cache?: Cache;
 }
 
 export interface Type<MModule extends Module = Module>
   extends TypeVersion<MModule>,
     Scoped {
   name: string;
-}
-
-export function isType<T extends Type>(type: unknown): type is T {
-  return (type as T).name ? true : false;
 }
