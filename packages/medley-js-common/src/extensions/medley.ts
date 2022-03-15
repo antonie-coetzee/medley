0b6ab72cae8056d599a20ef7ea776691e9b0ce90
antonie-coetzee <mail@acoetzee.com>
2022-03-15 10:15:35 +0200

    wip

--- packages/medley-js-common/src/extensions/medley.ts
+++ packages/medley-js-common/src/extensions/medley.ts
@@ -7,7 +7,7 @@ export type MedleyContext = {
 
 declare module "@medley-js/core" {
   interface Medley {
-    createContext: <T>(value: T) => MedleyContext;
+    createContext: <T>(key?: symbol, value?: T) => MedleyContext;
     setContext: <T>(context: MedleyContext, value: T) => void;
     useContext: <T>(context: MedleyContext) => T | undefined;
     context: { [index: symbol]: unknown };
@@ -19,17 +19,26 @@ declare module "@medley-js/core" {
 Medley.prototype.context = {};
 
 Medley.prototype.createContext = function <T>(value?: T) {
-  const symbol = Symbol();
-  this.context[symbol] = value;
-  return { key: symbol };
+  const contextKey = Symbol();
+  ensureContext(this);
+  this.context[contextKey] = value;
+  return { key: contextKey };
 };
 
 Medley.prototype.setContext = function <T>(context: MedleyContext, value: T) {
+  ensureContext(this);
   this.context[context.key] = value;
 };
 
 Medley.prototype.useContext = function <T>(context: MedleyContext) {
+  ensureContext(this);
   return this.context[context.key] as T | undefined;
 };
 
 Medley.prototype.logger = nullLogger;
+
+function ensureContext(instance: Medley){
+  if(instance.context == null){
+    instance.context = {};
+  }
+}
